67ac7ae828d21eff9585f4a04996ac88626322c4
sorrow_scavenger <114684909+htouil@users.noreply.github.com>
2023-03-10 21:55:52 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,56 +12,55 @@
 
 #include "get_next_line.h"
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-    int i;
-    if (!str)
-        return 0;
+	int	i;
 
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    return (i);
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
 
-char    *ft_strjoin(char *str1, char *str2)
+char	*ft_strjoin(char *str1, char *str2)
 {
-    int     i;
-    int     j;
-    char    *joined;
+	int		i;
+	int		j;
+	char	*joined;
 
-    joined = malloc((ft_strlen(str1) + ft_strlen(str2) + 1) * sizeof(char));
-    if (!joined)
-        return (NULL);
-    i = 0;
-    j = 0;
-    while (i < ft_strlen(str1))
-    {
-        joined[i] = str1[i];
-        i++;
-    }
-    while (j < ft_strlen(str2))
-    {
-        joined[i] = str2[j];
-        i++;
-        j++;
-    }
-    joined[i] = '\0';
-    return (joined);
+	if (!str1)
+	{
+		str1 = malloc(1 * sizeof(char));
+		str1[0] = '\0';
+	}
+	joined = malloc((ft_strlen(str1) + ft_strlen(str2) + 1) * sizeof(char));
+	if (!joined)
+		return (NULL);
+	i = -1;
+	j = 0;
+	while (str1[++i])
+		joined[i] = str1[i];
+	while (str2[j])
+		joined[i++] = str2[j++];
+	joined[i] = '\0';
+	free (str1);
+	return (joined);
 }
 
-int ft_strchr(char *searched)
+int	ft_strchr(char *searched)
 {
-    int i;
+	int	i;
 
-    if (!searched)
-        return (0);
-    i = 0;
-    while(searched[i] != '\0')
-    {
-        if (searched[i] == '\n')
-            return (1);
-        i++;
-    }
-    return (0);
+	if (!searched)
+		return (0);
+	i = 0;
+	while (searched[i] != '\0')
+	{
+		if (searched[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
 }
